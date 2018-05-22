c89d4e30d2d8a65197598a08f1186a2526349096
bernovici <bernovici@gmail.com>
2018-05-22 16:48:50 +0300

    Added one more endpoint for Grakn, to get similar animes by title

--- src/main/java/com/anime/rest/RestResource.java
+++ src/main/java/com/anime/rest/RestResource.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by bbernovici on 22.05.2018.
@@ -31,6 +28,7 @@ public class RestResource {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    //localhost:8080/anime/2273
     @RequestMapping(value="/anime/{id}",
         method = RequestMethod.GET)
     public ResponseEntity<?> getAnimeTitleByIdFromGrakn(@PathVariable String id) {
@@ -43,11 +41,27 @@ public class RestResource {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    //localhost:8080/animes/similar-to/2273
     @RequestMapping(value="/animes/similar-to/{id}",
             method = RequestMethod.GET)
     public ResponseEntity<?> getSimilarAnimesByIdFromGrakn(@PathVariable String id) {
         QueryBuilder qb = GraknConnector.getSessionToWrite().graql();
         GetQuery query = qb.match(var("x").isa("anime").has("anime_id", id), var().rel("x").rel("y").isa("similar_anime"), var("y").has("title", var("y_title"))).limit(2).get();
+        System.out.println(query.toString());
+        System.out.println(query.vars());
+
+        for(Answer ans : query) {
+            System.out.println(ans.get("y_title").asAttribute().getValue());
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    //localhost:8080/animes/similar-to?title=mobile suit gundam unicorn
+    @RequestMapping(value="/animes/similar-to",
+            method = RequestMethod.GET)
+    public ResponseEntity<?> getSimilarAnimesByTitleFromGrakn(@RequestParam String title) {
+        QueryBuilder qb = GraknConnector.getSessionToWrite().graql();
+        GetQuery query = qb.match(var("x").isa("anime").has("title", title), var().rel("x").rel("y").isa("similar_anime"), var("y").has("title", var("y_title"))).limit(5).get();
         System.out.println(query.toString());
         System.out.println(query.vars());
 
